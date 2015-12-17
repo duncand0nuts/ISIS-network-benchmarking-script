0f89c9be640065c643fcc7d8b6bb70e32b5dd25f
duncand0nuts <angusmaidment@googlemail.com>
2015-12-17 18:31:07 +0000

    added time functions and print statement to verify results

--- ISIS-network-benchmarking-script.py
+++ ISIS-network-benchmarking-script.py
@@ -7,10 +7,18 @@ def file_name_reader(input_file_path):
         filenames.append(line)
     return filenames
 
-filenames = file_name_reader('files.txt')
+filenames = file_name_reader('filename here')
 
 def grab_files(filenames):
+    times = []
     for file in filenames:
+        start = time.clock()
+
         load_file = Load(Filename = file)
 
+        elapsed = (time.clock() - start)
+        times.append(elapsed)
+    return(times)
+
 test = grab_files(filenames)
+print test
